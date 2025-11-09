f5e42512a6e60ead4d2449d3a336d82a345583aa
Likegenm <Oleg.25478@mail.ru>
2025-11-09 19:36:46 +0300

    Test Likegenm
    IDK

--- Tsb.lua
+++ Tsb.lua
@@ -1,3 +1,13 @@
-while true do
-  print(1)
-  wait(1)
+print("Test")
+local a = Instance.new("ScreenGui")
+a.Name = " "
+a.ResetOnSpawn = false
+
+local b = Instance.new("Frame")
+b.Size = UDim.new(10)
+b.Position = UDim2.new(0, 0)
+
+local c = Instance.new("TextButton")
+c.Text = "Likegenm test"
+c.Position = UDim2.new(10, 100)
+c.Size = UDim.new(50)
