6c332d312502a5325052341bbdfe0e8019e820d1
Likegenm <Oleg.25478@mail.ru>
2025-11-30 08:58:37 +0300

    Update Intruder(Home).lua

--- Intruder(Home).lua
+++ Intruder(Home).lua
@@ -24,4 +24,103 @@ local MainLoadButton = MainTab:AddButton({
     end    
 })
 
+local NoDurationTab = Window:MakeTab({
+    Name = "NoDuration",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local NoDurationToggle = NoDurationTab:AddToggle({
+    Name = "NoDuration",
+    Default = false,
+    Save = true,
+    Flag = "NoDuration",
+    Callback = function(Value)
+        if Value then
+            if workspace:FindFirstChild("Map") then
+                local map = workspace.Map
+                
+                if map:FindFirstChild("LightSwitch") then
+                    local lightSwitch = map.LightSwitch
+                    
+                    if lightSwitch:FindFirstChild("DownSwitch") then
+                        local downSwitch = lightSwitch.DownSwitch
+                        if downSwitch:FindFirstChild("ProximityPrompt") then
+                            local prompt = downSwitch.ProximityPrompt
+                            if prompt:FindFirstChild("Data") then
+                                prompt.Data.HoldDuration = 0
+                            end
+                        end
+                    end
+                    
+                    if lightSwitch:FindFirstChild("UpSwitch") then
+                        local upSwitch = lightSwitch.UpSwitch
+                        if upSwitch:FindFirstChild("ProximityPrompt") then
+                            local prompt = upSwitch.ProximityPrompt
+                            if prompt:FindFirstChild("Data") then
+                                prompt.Data.HoldDuration = 0
+                            end
+                        end
+                    end
+                end
+                
+                if map:FindFirstChild("Phone") then
+                    local phone = map.Phone
+                    if phone:FindFirstChild("Speaker") then
+                        local speaker = phone.Speaker
+                        
+                        local prompts = {
+                            "AnswerGuide",
+                            "AnswerGuide2", 
+                            "CallPolice",
+                            "FixPhone"
+                        }
+                        
+                        for _, promptName in pairs(prompts) do
+                            if speaker:FindFirstChild(promptName) then
+                                local prompt = speaker[promptName]
+                                if prompt:FindFirstChild("Data") then
+                                    prompt.Data.HoldDuration = 0
+                                end
+                            end
+                        end
+                    end
+                end
+                
+                if map:FindFirstChild("ClosetDoor") then
+                    local closetDoor = map.ClosetDoor
+                    if closetDoor:FindFirstChild("Handle") then
+                        local handle = closetDoor.Handle
+                        
+                        local actions = {"Close", "Open"}
+                        
+                        for _, action in pairs(actions) do
+                            if handle:FindFirstChild(action) then
+                                local actionPart = handle[action]
+                                if actionPart:FindFirstChild("Data") then
+                                    actionPart.Data.HoldDuration = 0
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            
+            OrionLib:MakeNotification({
+                Name = "NoDuration",
+                Content = "NoDuration enabled!",
+                Image = "rbxassetid://4483345998",
+                Time = 3
+            })
+        else
+            OrionLib:MakeNotification({
+                Name = "NoDuration", 
+                Content = "NoDuration disabled!",
+                Image = "rbxassetid://4483345998",
+                Time = 3
+            })
+        end
+    end    
+})
+
 OrionLib:Init()
