af92f75c318b89c3afc6a440abe4f4b698abfeac
Likegenm <Oleg.25478@mail.ru>
2025-12-06 10:07:26 +0300

    Update SK.lua

--- SK.lua
+++ SK.lua
@@ -52,3 +52,66 @@ LeftGroup:AddToggle("InfJumpToggle", {
         end
     end
 })
+
+LeftGroup:AddToggle("Fly", {
+    Text = "Fly",
+    Default = false,
+    
+    Callback = function(Value)
+        getgenv().FlyEnabled = Value
+        local UserInputService = game:GetService("UserInputService")
+        local RunService = game:GetService("RunService")
+        
+        if Value then
+            local player = game.Players.LocalPlayer
+            local character = player.Character or player.CharacterAdded:Wait()
+            local humanoid = character:WaitForChild("Humanoid")
+            local root = character:WaitForChild("HumanoidRootPart")
+            
+            local flySpeed = 50
+            local flyConnection
+            
+            flyConnection = RunService.Heartbeat:Connect(function()
+                if not getgenv().FlyEnabled then
+                    flyConnection:Disconnect()
+                    return
+                end
+                
+                local velocity = Vector3.new(0, 0, 0)
+                
+                if UserInputService:IsKeyDown(Enum.KeyCode.W) then
+                    velocity = velocity + root.CFrame.LookVector * flySpeed
+                end
+                if UserInputService:IsKeyDown(Enum.KeyCode.S) then
+                    velocity = velocity - root.CFrame.LookVector * flySpeed
+                end
+                if UserInputService:IsKeyDown(Enum.KeyCode.A) then
+                    velocity = velocity - root.CFrame.RightVector * flySpeed
+                end
+                if UserInputService:IsKeyDown(Enum.KeyCode.D) then
+                    velocity = velocity + root.CFrame.RightVector * flySpeed
+                end
+                if UserInputService:IsKeyDown(Enum.KeyCode.Space) then
+                    velocity = velocity + Vector3.new(0, flySpeed, 0)
+                end
+                if UserInputService:IsKeyDown(Enum.KeyCode.LeftShift) then
+                    velocity = velocity - Vector3.new(0, flySpeed, 0)
+                end
+                
+                root.Velocity = velocity
+                humanoid.PlatformStand = true
+            end)
+            
+            getgenv().FlyConnection = flyConnection
+        else
+            if getgenv().FlyConnection then
+                getgenv().FlyConnection:Disconnect()
+            end
+            
+            local player = game.Players.LocalPlayer
+            if player.Character and player.Character:FindFirstChild("Humanoid") then
+                player.Character.Humanoid.PlatformStand = false
+            end
+        end
+    end
+})
