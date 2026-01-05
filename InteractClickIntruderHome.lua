53657b1702da2c24bf159ebc88181717110c6609
Likegenm <Oleg.25478@mail.ru>
2026-01-05 19:02:51 +0300

    Create InteractClickIntruderHome.lua

---
+++ InteractClickIntruderHome.lua
@@ -0,0 +1,7 @@
+game.workspace.Map.Phone.Speaker.AnswerGuide.HoldDuration = 0
+game.workspace.Map.Phone.Speaker.AnswerGuide2.HoldDuration = 0
+game.workspace.Map.Phone.Speaker.FixPhone.HoldDuration = 0
+game.workspace.Map.LightSwitch.DownSwitch.ProximityPrompt.HoldDuration = 0
+game.workspace.Map.LightSwitch.UpSwitch.ProximityPrompt.HoldDuration = 0
+game.workspace.Map.ClosetDoor.Handle.Close.HoldDuration = 0
+game.workspace.Map.ClosetDoor.Handle.Open.HoldDuration = 0
