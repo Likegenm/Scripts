14444e46fd4c2e6b3938aad8cd0e541bf222257f
Likegenm <Oleg.25478@mail.ru>
2026-01-06 11:01:51 +0300

    Update HomePosIntruder.lua

--- HomePosIntruder.lua
+++ HomePosIntruder.lua
@@ -10,56 +10,57 @@ local function showNotification(titleText, subtitleText)
     
     local frame = Instance.new("Frame")
     frame.Size = UDim2.new(0, 300, 0, 75)
-    frame.Position = UDim2.new(1, 10, 1, -110) -- За экраном справа
+    frame.Position = UDim2.new(1, 10, 1, -110)
     frame.BackgroundColor3 = Color3.new(0, 0, 0)
     frame.BackgroundTransparency = 0.3
     frame.BorderSizePixel = 0
+    frame.Parent = screenGui
     
-    local corner = Instance.new("UICorner", frame)
+    local corner = Instance.new("UICorner")
     corner.CornerRadius = UDim.new(0, 5)
+    corner.Parent = frame
     
-    local title = Instance.new("TextLabel", frame)
-    title.Text = titleText or "Hello"
+    local title = Instance.new("TextLabel")
+    title.Text = tostring(titleText) or "Hello"
     title.Size = UDim2.new(1, 0, 0.5, 0)
     title.BackgroundTransparency = 1
     title.TextColor3 = Color3.new(1, 1, 1)
     title.Font = Enum.Font.GothamBold
     title.TextSize = 28
+    title.Parent = frame
     
-    local subtitle = Instance.new("TextLabel", frame)
-    subtitle.Text = subtitleText or "Hi"
+    local subtitle = Instance.new("TextLabel")
+    subtitle.Text = tostring(subtitleText) or "Hi"
     subtitle.Size = UDim2.new(1, 0, 0.5, 0)
     subtitle.Position = UDim2.new(0, 0, 0.5, 0)
     subtitle.BackgroundTransparency = 1
     subtitle.TextColor3 = Color3.new(1, 1, 1)
     subtitle.Font = Enum.Font.Gotham
     subtitle.TextSize = 20
+    subtitle.Parent = frame
     
-    frame.Parent = screenGui
     screenGui.Parent = PlayerGui
     
     local TweenService = game:GetService("TweenService")
     
     local appear = TweenService:Create(frame, 
         TweenInfo.new(0.5, Enum.EasingStyle.Back, Enum.EasingDirection.Out), {
-            Position = UDim2.new(1, -310, 1, -110) -- В правый нижний угол
+            Position = UDim2.new(1, -310, 1, -110)
         })
     
     local disappear = TweenService:Create(frame,
         TweenInfo.new(0.5, Enum.EasingStyle.Back, Enum.EasingDirection.In), {
-            Position = UDim2.new(1, 10, 1, -110) -- Обратно за экран
+            Position = UDim2.new(1, 10, 1, -110)
         })
     
     appear:Play()
     
-
     task.wait(3)
     
-    appear:Cancel()
     disappear:Play()
     
     disappear.Completed:Wait()
     screenGui:Destroy()
 end
 
-showNotificatio(IntruderPos:, game.workspace.Values.intruderPos.Value)
+showNotification("IntruderPos:", tostring(game.workspace.Values.intruderPos.Value))
