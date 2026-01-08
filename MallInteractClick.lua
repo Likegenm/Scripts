c82459e9a93deb355ea637f9f4f50c85c83c98bd
Likegenm <Oleg.25478@mail.ru>
2026-01-08 10:19:12 +0300

    Create MallInteractClick.lua

---
+++ MallInteractClick.lua
@@ -0,0 +1,8 @@
+game.workspace.Map.Phone.Phone.AnswerGuide.HoldDuration = 0
+game.workspace.Deactivators:FindFirstChild("1").Neon.ProximityPrompt.HoldDuration = 0
+game.workspace.Deactivators:FindFirstChild("2").Neon.ProximityPrompt.HoldDuration = 0
+game.workspace.Deactivators:FindFirstChild("3").Neon.ProximityPrompt.HoldDuration = 0
+game.workspace.Deactivators:FindFirstChild("4").Neon.ProximityPrompt.HoldDuration = 0
+game.workspace.Map.LightSwitch.DownSwitch.ProximityPrompt.HoldDuration = 0
+game.workspace.Map.LightSwitch.UpSwitch.ProximityPrompt.HoldDuration = 0
+game.workspace.Lockers.Locker.Door.ProximityPrompt.HoldDuration = 0
