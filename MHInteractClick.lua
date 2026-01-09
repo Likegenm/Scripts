6a4e311936d3e398924af6fd4ae7de1c85448945
Likegenm <Oleg.25478@mail.ru>
2026-01-09 10:53:23 +0300

    Create MHInteractClick.lua

---
+++ MHInteractClick.lua
@@ -0,0 +1,4 @@
+game.workspace.CellRoom.Closet.Extras.Generator.Interact.ProximityPrompt.HoldDuration = 0
+game.workspace.CellRoom.UnknownRoom.Wrench.ProximityPrompt.HoldDuration = 0
+game.workspace.CellRoom.Door.Prompt.Close.HoldDuration = 0
+game.workspace.CellRoom.Door.Prompt.Open.HoldDuration = 0
