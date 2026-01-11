9fbd283cb97661f6150f58b46e86cfc525a20a5a
Likegenm <Oleg.25478@mail.ru>
2026-01-11 12:04:57 +0300

    Update OAKInteractClick.lua

--- OAKInteractClick.lua
+++ OAKInteractClick.lua
@@ -1,1 +1,2 @@
 game:GetService("Workspace"):FindFirstChild("Lobby"):FIndFirstChild("Worker"):FindFirstChild("Torso"):FindFirstChild("ProximityPrompt"):FindFirstChild("HoldDuration") = 0
+game:GetService("Workspace"):FindFirstChild("Lobby"):FIndFirstChild("Doors"):FindFirstChild("SecretDoor"):FindFirstChild("Handle"):FindFirstChild("ProximityPrompt"):FindFirstChild("HoldDuration") = 0
