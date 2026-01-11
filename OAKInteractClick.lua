756f8ba628f0c5b47df3997b2050a91d065bd882
Likegenm <Oleg.25478@mail.ru>
2026-01-11 12:10:29 +0300

    Update OAKInteractClick.lua

--- OAKInteractClick.lua
+++ OAKInteractClick.lua
@@ -1,2 +1,6 @@
 game:GetService("Workspace"):FindFirstChild("Lobby"):FIndFirstChild("Worker"):FindFirstChild("Torso"):FindFirstChild("ProximityPrompt"):FindFirstChild("HoldDuration") = 0
 game:GetService("Workspace"):FindFirstChild("Lobby"):FIndFirstChild("Doors"):FindFirstChild("SecretDoor"):FindFirstChild("Handle"):FindFirstChild("ProximityPrompt"):FindFirstChild("HoldDuration") = 0
+game.workspace:WaitForChild("HotelRoom")
+if game.workspace.HotelRoom then
+  game.workspace.HotelRoom.LightSwitcher.DownSwitch.ProximityPrompt.HoldDuration = 0
+  game.workspace.HotelRoom.LightSwitcher.UpSwitch.ProximityPrompt.HoldDuration = 0
