b3764bac2d749fb038f50ed0312ce9c9a34b4894
Likegenm <Oleg.25478@mail.ru>
2026-03-26 20:32:07 +0300

    Create Invis + Phantasm.lua

---
+++ Invis + Phantasm.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ATrainz/Phantasm/refs/heads/main/Games/TSB.lua"))()
+
+wait(1)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/InvisforPhantasm.lua"))()
